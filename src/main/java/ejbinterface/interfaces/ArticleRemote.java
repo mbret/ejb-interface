786480ccaa348cdd57d46f2a2edcdf94dc18bba3
unknown <bret.maxime@gmail.com>
2015-02-22 18:14:16 +0100

    maj

--- src/main/java/ejbinterface/interfaces/ArticleRemote.java
+++ src/main/java/ejbinterface/interfaces/ArticleRemote.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Remote
-public interface ArticleRemote {
+public interface ArticleRemote{
 	
     public ArticleShared save(String title, String content, Object userID);
     
