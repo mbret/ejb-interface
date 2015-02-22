799525e38c67dc2ffcb849d2a581c3a830a51ccf
unknown <bret.maxime@gmail.com>
2015-02-22 17:30:13 +0100

    maj

--- src/main/java/ejbinterface/interfaces/UserRemote.java
+++ src/main/java/ejbinterface/interfaces/UserRemote.java
@@ -10,9 +10,13 @@ public interface UserRemote{
 	// Authentification
 	public UserShared getUser(String mail, String password);
 
+    public UserShared findOne(Object id);
+    
 	// Inscription
 	public UserShared save(String mail, String password);
 
+    public void update(UserShared user);
+    
     public boolean emailExist(String email);
     
     public boolean test();
